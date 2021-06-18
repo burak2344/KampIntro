4acb7fa8369626a5cac6b99168c0cd06a97b669f
Burak Yunkul <byunkul23@hotmail.com>
2021-06-18 17:22:09 +0300

    18 Haziran GenericsMyDictionary Çalışması

---
+++ GenericsMyDictionary/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace GenericsMyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<string> cars = new MyDictionary<string>();
+            cars.Add("Fiat Egea");
+            cars.Add("Opel Corsa");
+            cars.Add("Bmw A102");
+            cars.Add("Honda Civic");
+            Console.WriteLine(cars.Count);
+        }
+    }
+}
