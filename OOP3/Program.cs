1447ef71c50b2a2f23b22b5ad9e57d0331b5699d
Burak Yunkul <byunkul23@hotmail.com>
2021-06-22 15:39:21 +0300

    22 Haziran Ders Sonu İnterface-Implements- Inheritance Çalışması

---
+++ OOP3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            IKrediManager esnafKrediManager = new EsnafKredisiManager();
+
+            ILoggerService databaseLoggerService = new DatabaseLoggerService();
+            ILoggerService fileLoggerService = new FileLoggerService();
+            ILoggerService smsLoggerService = new SmsLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(ihtiyacKrediManager, databaseLoggerService);
+            basvuruManager.BasvuruYap(esnafKrediManager, fileLoggerService);
+            basvuruManager.BasvuruYap(tasitKrediManager, smsLoggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ihtiyacKrediManager,tasitKrediManager };
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+
+
+        }
+    }
+}
