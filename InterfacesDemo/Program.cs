2878d925a384ab1aa0983a01a66bb10bd73d7fc4
Burak Yunkul <byunkul23@hotmail.com>
2021-06-26 16:31:39 +0300

    26 Haziran 2021 InterfacesDemo (Çoklu Implementation)

---
+++ InterfacesDemo/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+
+namespace InterfacesDemo
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IWorker[] workers = new IWorker[3]
+			{
+				new Manager(),
+				new Worker(),
+				new Robot(),
+			};
+
+			foreach (var worker in workers)
+			{
+				worker.Work();
+			}
+
+			IEat[] eat = new IEat[2]
+			{
+				new Manager(),
+				new Worker(),
+			};
+
+			foreach (var eats in eat)
+			{
+				eats.Eat();
+			}
+		}
+	}
+
+	interface IWorker
+	{
+		void Work();
+
+	}
+
+	interface IEat
+	{
+		void Eat();
+	}
+
+	interface ISalary
+	{
+		void GetSalary();
+	}
+
+	class Manager : IWorker, ISalary, IEat
+	{
+		public void Eat()
+		{
+			Console.WriteLine("Manager Eat");
+		}
+
+		public void GetSalary()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Work()
+		{
+			Console.WriteLine("Manager Work");
+		}
+	}
+
+	class Worker : IWorker, ISalary, IEat
+	{
+        public void Eat()
+		{
+			Console.WriteLine("Worker Eat");
+		}
+
+		public void GetSalary()
+		{
+			throw new NotImplementedException();
+		}
+
+		public void Work()
+		{
+			Console.WriteLine("Worker Work");
+		}
+	}
+
+	class Robot : IWorker
+	{
+		public void Work()
+		{
+			Console.WriteLine("Robot Work");
+		}
+	}
+}
