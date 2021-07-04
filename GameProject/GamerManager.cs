1b0049b644ddd44de5ecf0e408db58f9ae715bfe
Burak Yunkul <byunkul23@hotmail.com>
2021-07-04 22:57:21 +0300

    4 Temmuz 2021

---
+++ GameProject/GamerManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameProject
+{
+	class GamerManager : IGamerService
+	{
+		IUserValidationService _uservalidationService;
+        public GamerManager(IUserValidationService uservalidationService)
+		{
+			this._uservalidationService = uservalidationService;
+		}
+
+		public void Add(Gamer gamer)
+		{
+			if (_uservalidationService.Validate(gamer))
+			{
+				Console.WriteLine("Kayıt oldu");
+			}
+			else
+			{
+				Console.WriteLine("Doğrulama ve kayıt başarısız.");
+			}
+			
+		}
+
+		public void Delete(Gamer gamer)
+		{
+			Console.WriteLine("Kayıt silindi");
+		}
+
+		public void Update(Gamer gamer)
+		{
+			Console.WriteLine("Kayıt güncellendi");
+		}
+	}
+}
---
+++ GameProject/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace GameProject
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			GamerManager gamerManager = new GamerManager(new NewEStateUserValidationManager());
+			gamerManager.Add(new Gamer
+			{
+				Id = 1,
+				FirstName = "BURAK",
+				LastName = "YUNKUL",
+				IdentityNumber = 123456,
+				BirthYear = 1985
+			});
+		}
+	}
+}
