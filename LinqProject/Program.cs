554e385969061c6a5fe98f38ffbf67dd3d8282be
Burak Yunkul <byunkul23@hotmail.com>
2021-07-14 18:46:40 +0300

    14 Temmuz 2021 Linq Kullanımı Test

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -17,16 +17,72 @@ namespace LinqProject
 			List<Product> products = new List<Product>
 			{
 				new Product{ProductId=1,CategoryId=1,ProductName="Acer Laptop",QuantityPerUnit="32 Gb Ram",UnitPrice=10000,UnitsInStock=5},
-				new Product{ProductId=2,CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit="16 Gb Ram",UnitPrice=8000,UnitsInStock=3},
-				new Product{ProductId=3,CategoryId=1,ProductName="Hp Laptop",QuantityPerUnit="8 Gb Ram",UnitPrice=6000,UnitsInStock=2},
+				new Product{ProductId=2,CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit="16 Gb Ram",UnitPrice=18000,UnitsInStock=3},
+				new Product{ProductId=3,CategoryId=1,ProductName="Hp Laptop",QuantityPerUnit="8 Gb Ram",UnitPrice=18000,UnitsInStock=2},
 				new Product{ProductId=4,CategoryId=2,ProductName="Samsung Telefon",QuantityPerUnit="4 Gb Ram",UnitPrice=5000,UnitsInStock=15},
-				new Product{ProductId=5,CategoryId=2,ProductName="AApple Telefon",QuantityPerUnit="4 Gb Ram",UnitPrice=8000,UnitsInStock=0},
+				new Product{ProductId=5,CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit="4 Gb Ram",UnitPrice=8000,UnitsInStock=0},
 			};
+			//Test(products);
 
+			//GetProducts(products);
+
+			//AnyTest(products);
+
+			//FindTest(products);
+
+			//FindAllTest(products);
+
+			//AscDescTest(products);
+			//LinqTest(products);
+
+		}
+
+		private static void LinqTest(List<Product> products)
+		{
+			var result = from p in products
+						 where p.UnitPrice > 6000
+						 orderby p.UnitPrice descending, p.ProductName ascending
+						 select p;
+			foreach (var product in result)
+			{
+				Console.WriteLine(product.ProductName);
+			}
+		}
+
+		private static void AscDescTest(List<Product> products)
+		{
+			//Single line query
+			var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+			foreach (var product in result)
+			{
+				Console.WriteLine(product.ProductName);
+			}
+		}
+
+		private static void FindAllTest(List<Product> products)
+		{
+			var result = products.FindAll(p => p.ProductName.Contains("top"));
+			Console.WriteLine(result);
+		}
+
+		private static void FindTest(List<Product> products)
+		{
+			var result = products.Find(p => p.ProductId == 3);
+			Console.WriteLine(result.ProductName);
+		}
+
+		private static void AnyTest(List<Product> products)
+		{
+			var result = products.Any(p => p.ProductName == "Acer Laptop");
+			Console.WriteLine(result);
+		}
+
+		private static void Test(List<Product> products)
+		{
 			Console.WriteLine("Algoritmik-------------------------------");
 			foreach (var product in products)
 			{
-				if (product.UnitPrice>5000 && product.UnitsInStock>3)
+				if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
 				{
 					Console.WriteLine(product.ProductName);
 				}
@@ -34,14 +90,11 @@ namespace LinqProject
 
 			Console.WriteLine("Linq-------------------------------");
 
-			var result = products.Where(p=> p.UnitPrice > 5000 && p.UnitsInStock > 3);
+			var result = products.Where(p => p.UnitPrice > 5000 && p.UnitsInStock > 3);
 			foreach (var product in result)
 			{
 				Console.WriteLine(product.ProductName);
 			}
-
-			GetProducts(products);
-
 		}
 
 		static List<Product> GetProducts(List<Product> products)
